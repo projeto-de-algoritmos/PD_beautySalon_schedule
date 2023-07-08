d52d2844555aa9100a739bc27fb98093b6c00f40
Mayara Alves de Oliveira <67807684+Mayara-tech@users.noreply.github.com>
2023-07-07 22:03:34 -0300

    correcao nos nomes
    Co-authored-by: MIllena Queiroz <millena.queiroz08@gmail.com>

--- agenda_salao.py
+++ agenda_salao.py
@@ -94,7 +94,7 @@ def handle_submit():
 
 
 def format_result(result):
-    formatted = "Intervalos agendados:\n"
+    formatted = "Agendamento:\n"
     for interval in result:
         formatted += f"Nome: {interval[3]}, Descrição: {interval[4]}, Início: {interval[0]}, Término: {interval[1]}, Valor: {interval[2]}\n"
     return formatted
@@ -102,13 +102,13 @@ def format_result(result):
 
 # Interface Gráfica
 root = tk.Tk()
-root.title("Agendamento de Intervalos")
+root.title("Beauty Salon Schedule")
 root.geometry("400x400")
 
 frame = tk.Frame(root)
 frame.pack(pady=20)
 
-label_num_intervals = tk.Label(frame, text="Número de Intervalos:")
+label_num_intervals = tk.Label(frame, text="Número de Agendamentos:")
 label_num_intervals.pack()
 
 entry_num_intervals = tk.Entry(frame)
@@ -127,35 +127,35 @@ def create_interval_fields():
     num_intervals = int(entry_num_intervals.get())
 
     for i in range(num_intervals):
-        label_name = tk.Label(frame_intervals, text=f"Nome Intervalo {i+1}:")
+        label_name = tk.Label(frame_intervals, text=f"Nome da Cliente {i+1}:")
         label_name.pack()
 
         entry_name = tk.Entry(frame_intervals)
         entry_name.pack()
         entry_names.append(entry_name)
 
-        label_description = tk.Label(frame_intervals, text=f"Descrição Intervalo {i+1}:")
+        label_description = tk.Label(frame_intervals, text=f"Descrição {i+1}:")
         label_description.pack()
 
         entry_description = tk.Entry(frame_intervals)
         entry_description.pack()
         entry_descriptions.append(entry_description)
 
-        label_start = tk.Label(frame_intervals, text=f"Início Intervalo {i+1}:")
+        label_start = tk.Label(frame_intervals, text=f"Início {i+1}:")
         label_start.pack()
 
         entry_start = tk.Entry(frame_intervals)
         entry_start.pack()
         entry_starts.append(entry_start)
 
-        label_end = tk.Label(frame_intervals, text=f"Término Intervalo {i+1}:")
+        label_end = tk.Label(frame_intervals, text=f"Término {i+1}:")
         label_end.pack()
 
         entry_end = tk.Entry(frame_intervals)
         entry_end.pack()
         entry_ends.append(entry_end)
 
-        label_value = tk.Label(frame_intervals, text=f"Valor Intervalo {i+1}:")
+        label_value = tk.Label(frame_intervals, text=f"Valor {i+1}:")
         label_value.pack()
 
         entry_value = tk.Entry(frame_intervals)
